9a5cc08110df500ffb7e1535a2e12ac67784eddf
Sudip Diliprao Kumbhakarn <118417926+Sudip-Diliprao-Kumbhakarn@users.noreply.github.com>
2025-12-15 16:15:59 +0530

    rewrite the code

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-function getValue(params) {
-    let location = document.getElementById("location");
-    let value = location.value;
-    console.log(value);
-}
+const button = document.getElementById("searchBtn");
 
-fatch(`api.openweathermap.org/data/2.5/weather?q=${value}&appid=your_api_key`).then(response => response.jeson()).then(
-    displayData
-).catch(err => alert('wrong city name'));console.log(displayData);
+button.addEventListener("click", function () {
+  const city = document.getElementById("cityInput").value;
+
+  if (city === "") {
+    alert("Please enter a city name");
+    return;
+  }
+
+  getWeather(city);
+});
+
+function getWeather(city) {
+  const apiKey = "YOUR_API_KEY";
+  //const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${city}&lon=${apiKey}&appid={API key}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("cityName").innerText = data.name;
+      document.getElementById("temperature").innerText = "Temp: " + data.main.temp + " °C";
+      document.getElementById("condition").innerText = "Weather: " + data.weather[0].main;
+      document.getElementById("humidity").innerText = "Humidity: " + data.main.humidity + "%";
+      document.getElementById("wind").innerText = "Wind: " + data.wind.speed + " m/s";
+    })
+    .catch(error => {
+      alert("City not found");
+    });
+}
